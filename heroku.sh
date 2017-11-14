11df165cf0a2ea010f330906809ff0bde9e0d454
rorykoehein <rory@intelworks.com>
2017-11-14 22:20:12 +0100

    auto deploy $1

--- heroku.sh
+++ heroku.sh
@@ -2,9 +2,8 @@
 
 echo "Deploy to heroku"
 
-./build.sh $1
-cd ../dist/$1
-cp ./Procfile ../dist/$1/
+cp ./Procfile ../dist/
+cd ../dist/
 git add .
 git commit -m 'auto deploy $1'
 git push heroku master
