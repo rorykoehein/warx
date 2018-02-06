2d78190e287e3d64950b90d44a45411c4f35eab3
rorykoehein <rory@intelworks.com>
2018-02-06 22:18:14 +0100

    setup server module on client

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,7 @@
 import io from 'socket.io-client';
 import { pack, unpack, packKey } from './shared/pack-messages';
 
-const address = process.env.NODE_ENV === 'development' ? `http://${window.location.hostname}:5000` : null;
+const address = process.env.REACT_APP_SOCKET_ADDRESS || undefined;
 const socket = io(address, /*{ transports: ['polling']}*/);
 
 export const onNetworkAction = (callback: Function) => {
